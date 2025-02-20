6a1a9c69f6f8505ad92f80925b41d363bbe38a49
sunnyfromomori69 <goldenluffy127@gmail.com>
2025-02-20 14:27:56 +0400

    commit

---
+++ day 04/homework/homework1.py
@@ -0,0 +1,3 @@
+mother = "izi pirveli"
+sister = "barbare bazandarashvili"
+print("i love my mother izi pirveli" + " " + "she does so much for me and it makes me happy" + " i really appretiate everything she does")
---
+++ day 04/homework/homework2.py
@@ -0,0 +1,4 @@
+print(1 + 1)
+print( 20 - 10)
+print( 50 * 100)
+print( 100 // 50)
